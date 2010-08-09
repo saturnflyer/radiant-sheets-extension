932503335b2af4ce990315a21e6c922942a9491d
Jim Gay <jim@saturnflyer.com>
2010-08-08 22:23:59 -0400

    cleaner page spec

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -1,9 +1,5 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Page do
-  describe '#sheet?' do
-    it "should return false" do
-      Page.new.sheet?.should be_false
-    end
-  end
+  its(:sheet?){should be_false}
 end
