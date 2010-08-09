712fab8856bb99347107d032a279700a8736e9de
Jim Gay <jim@saturnflyer.com>
2010-08-08 21:14:23 -0400

    more specs for stylesheet saving

--- spec/models/stylesheet_page_spec.rb
+++ spec/models/stylesheet_page_spec.rb
@@ -53,4 +53,28 @@ describe StylesheetPage do
       j.breadcrumb.should == 'site.css'
     end
   end
+  
+  context 'when saving a new page' do
+    subject { s = StylesheetPage.new_with_defaults
+        s.slug = 'test.css'
+        s.save!
+        s }
+    its(:status){ should == Status[:published] }
+    its(:status_id){ should == Status[:published].id }
+    
+    its(:published_at){ should_not be_nil }
+    it "should have a published_at greater than or equal to the current time" do
+      subject.published_at.to_i.should <= Time.zone.now.to_i
+    end
+    
+    context 'with the default page status set to draft' do
+      it 'should save a new page with a published status' do
+        Radiant::Config['defaults.page.status'] = 'draft'
+        new_sheet = StylesheetPage.new_with_defaults
+        new_sheet.slug = 'published.css'
+        new_sheet.save!
+        new_sheet.status.should == Status[:published]
+      end
+    end
+  end
 end
