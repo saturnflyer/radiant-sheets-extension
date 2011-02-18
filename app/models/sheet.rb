8b5178c6f4c6f1b52154e28bf482dbabbe4b5cbc
Adam Tanner <adam@adamtanner.org>
2011-02-18 16:49:01 -0500

    find_by_path should be find_by_url

--- app/models/sheet.rb
+++ app/models/sheet.rb
@@ -6,38 +6,38 @@ module Sheet
         before_validation :set_breadcrumb
         before_validation :set_published
         class_inheritable_accessor :sheet_root
-        
+
         def self.root
-          sheet_root ||= Page.find_by_path('/').children.first(:conditions => {:class_name => self.to_s})
+          sheet_root ||= Page.find_by_url('/').children.first(:conditions => {:class_name => self.to_s})
         end
 
         def self.default_page_parts
           PagePart.new(:name => 'body')
         end
-        
+
         def self.create_from_upload!(file)
           @sheet = self.new_with_defaults
           @sheet.upload = file
           @sheet.save!
           @sheet
         end
-        
+
       }
     end
-    
+
     def cache?
       true
     end
-  
+
     # so that it is ignored when finding children in radius tags
     def virtual?
       true
     end
-  
+
     def layout
       nil
     end
-    
+
     def find_by_url(url, live = true, clean = true)
       url = clean_url(url) if clean
       my_url = self.url
@@ -52,7 +52,7 @@ module Sheet
         end
       end
     end
-        
+
     def upload=(file)
       case
       when file.blank?
@@ -62,21 +62,20 @@ module Sheet
       when file.size > 262144 # 256k (that's a HUGE script or stylesheet)
         self.errors.add(:upload, 'file size is larger than 256kB. Please upload a smaller file.')
       else
-        self.slug = file.original_filename.to_slug().gsub(/-css$/,'.css').gsub(/-js/,'.js')
         self.part('body').content = file.read
       end
     end
-  
+
     private
-  
+
     def set_title
       self.title = self.slug
     end
-  
+
     def set_breadcrumb
       self.breadcrumb = self.slug
     end
-    
+
     def set_published
       self.published_at = Time.now
       self.status_id = Status[:published].id
