cdcb34262d66885dd88a98c08a7f5b6eb46d7762
johnmuhl <git@johnmuhl.com>
2011-08-24 12:05:09 -0500

    change require to work on 1.9.2 and 1.8.7

--- spec/lib/stylesheet_tags_spec.rb
+++ spec/lib/stylesheet_tags_spec.rb
@@ -1,5 +1,5 @@
 # require 'spec_helper'
-require File.dirname(__FILE__) + '/../spec_helper'
+require File.expand_path("../../spec_helper", __FILE__)
 
 describe "Stylesheet Tags" do
   dataset :stylesheets
