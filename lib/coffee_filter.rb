286e851f6abe716a585128172b0a339d9f16c2ff
Jim Gay <jim@saturnflyer.com>
2012-02-25 23:45:40 -0500

    attempt to gracefully fail without coffee script

--- lib/coffee_filter.rb
+++ lib/coffee_filter.rb
@@ -1,6 +1,11 @@
-require 'coffee_script'
-class CoffeeFilter < TextFilter
-  def filter(text)
-    CoffeeScript.compile text
+begin
+  require 'coffee_script'
+  class CoffeeFilter < TextFilter
+    def filter(text)
+      CoffeeScript.compile text
+    end
   end
+rescue ExecJS::RuntimeUnavailable
+  Rails.logger.warn "There is no support for CoffeeScript"
+  class CoffeeFilter;end
 end
